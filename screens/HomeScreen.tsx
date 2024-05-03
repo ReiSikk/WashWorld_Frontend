3629f3a6197753772445761f97cac7b34cdf0648
Michalina <michalina.oniszczuk@gmail.com>
2024-05-03 15:28:09 +0200

    component added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Text } from 'native-base'
+import { Text, View } from 'native-base'
+import SubscriptionCard from '../components/SubscriptionCard'
 
 function HomeScreen() {
   return (
+    <View>
     <Text>HomeScreen</Text>
+    <SubscriptionCard></SubscriptionCard>
+    </View>
   )
 }
 

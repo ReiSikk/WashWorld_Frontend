39697a613e6d07d9ed65af1051fd5f0567daf5bf
Rei Sikk <sikkrei@gmail.com>
2024-05-07 13:44:52 +0200

    selected subscription stored in state, license plate screen setup

--- screens/subscriptionFlow/EnterLicensePlate.tsx
+++ screens/subscriptionFlow/EnterLicensePlate.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Text } from 'native-base'
+import React, { useState } from 'react';
+import { Box, VStack, HStack, Text, Input, Select, Button, IconButton, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AppDispatch } from '../../store/store';
+import { useDispatch } from 'react-redux';
 
-function EnterLicensePlate() {
+
+const EnterLicensePlate = () => {
+  const dispatch: AppDispatch = useDispatch();
+
   return (
-    <Text>EnterLicensePlate</Text>
-  )
-}
+    <VStack space={4} px="5" py="2">
+      <Text fontSize="xl" bold mt="5">
+        Enter your license plate
+      </Text>
 
-export default EnterLicensePlate
+      <Button mt="5" colorScheme="green" onPress={() => console.log('Confirm Pressed')}>
+        Confirm
+      </Button>
+    </VStack>
+  );
+};
+
+export default EnterLicensePlate;
